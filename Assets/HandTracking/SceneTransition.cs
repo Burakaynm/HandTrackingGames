ad2be348877fc79982f2ea984307a3b79ac8a23d
osmangurses <osmanileiletisim@gmail.com>
2024-06-08 23:31:24 +0300

    Scene Transition

---
+++ Assets/HandTracking/SceneTransition.cs
@@ -0,0 +1,19 @@
+using EasyTransition;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public static SceneTransition Instance;
+    [SerializeField] TransitionSettings transition;
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        TransitionManager.Instance().Transition(sceneName, transition, 0);
+    }
+}
